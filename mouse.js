02ecdb1e7f114e159cfddab53824d238c00eb6b7
José Valderrama <jvalderrama2602@gmail.com>
2016-09-29 18:47:30 -0400

    Clase carga de imagenes en Canvas

--- mouse.js
+++ mouse.js
@@ -3,73 +3,15 @@ function movimiento(eventos) {
     var y = eventos.clientY;
     var cuadro=document.getElementById("areaMouse");
 	var papel=cuadro.getContext("2d");
-
-    dibujar("blue",x+1,y+1,x-1,y-1,papel);
-    /*var coor = "Coordenadas: (" + x + "," + y + ")";*/
-    /*document.getElementById("demo").innerHTML = coor;*/
-    /*console.log(coor);*/
-    /*console.log(dibujar)*/
-    var xy = document.getElementById("coordenadas");
-    xy.innerHTML=(y)
+    dibujar("blue",x+1,y+1,x-1,y-1,papel);  
 }
-/*
-function limpiar() {
-    console.log("Salio");
-}*/
 
 function dibujar(color,xinicial,yinicial,xfinal,yfinal,lienzo)
 	{
-
 		lienzo.beginPath();
-
 		lienzo.strokeStyle=color;
-
 		lienzo.moveTo(xinicial,yinicial);
-
 		lienzo.lineTo(xfinal,yfinal);
-
 		lienzo.stroke();
-
 		lienzo.closePath();
-
-
-
 	}
-
-/*
-function dibujarTeclado(evento)
-{
-
-	var lineaColor="blue"
-	var movimiento=1
-
-
-
-	if(evento.keyCode==teclas.UP){
-		dibujar(lineaColor,x,y,x,y-movimiento,papel);
-		y=y-movimiento;
-
-	}
-
-	if(evento.keyCode==teclas.DOWN){
-		
-		dibujar(lineaColor,x,y,x,y+movimiento,papel);
-		y=y+movimiento;
-
-	}
-
-	if(evento.keyCode==teclas.LEFT){
-		dibujar(lineaColor,x,y,x-movimiento,y,papel);
-		x=x-movimiento;
-
-
-	}
-
-	if(evento.keyCode==teclas.RIGHT){
-		dibujar(lineaColor,x,y,x+movimiento,y,papel);
-		x=x+movimiento;
-
-
-	}
-};
-*/
