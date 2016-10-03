7cc7de6ce0e35d9374a001770f08c60360062424
José Valderrama <jvalderrama2602@gmail.com>
2016-10-03 15:18:58 -0400

    Clase de FizzBuzz con funciones.

--- fizz.js
+++ fizz.js
@@ -3,24 +3,36 @@ var divisible=false;
 for (var i =1; i<=100;i++)
 {
 	var divisible=false;
-	if(i%3==0)
+	if(esDivisible(i,3))
 	{
 		document.write("Fizz");
 		var divisible=true;
 	}
 
-	if(i%5==0)
+	if(esDivisible(i,5))
 	{
 		document.write("Buzz");	
 		var divisible=true;
 	}
 	
 
-	if(!divisible)
+	if(!esDivisible(i,3) && !esDivisible(i,5))
 	{
 		document.write(i);
 	}
 	document.write("<br>")
 
 
+}
+
+function esDivisible (num,divisor)
+{
+	if(num%divisor==0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
